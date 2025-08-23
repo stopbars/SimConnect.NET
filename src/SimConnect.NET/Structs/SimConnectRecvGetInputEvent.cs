18daa3e97cf4b8b65473fb52b2717ebc3c5f3770
bstudtma <bstudtma@gmail.com>
2025-08-23 03:16:50 -0500

    Add header structure and make Value Object type

--- src/SimConnect.NET/Structs/SimConnectRecvGetInputEvent.cs
+++ src/SimConnect.NET/Structs/SimConnectRecvGetInputEvent.cs
@@ -10,6 +10,27 @@ namespace SimConnect.NET
     public struct SimConnectRecvGetInputEvent
     {
         /// <summary>
+        /// Gets or sets the client-defined request ID.
+        /// </summary>
+        public uint RequestId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the input event. This is used to cast the Value to the correct type.
+        /// </summary>
+        public SimConnectInputEventType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the requested input event, which should be cast to the correct format (float / string).
+        /// </summary>
+        public Object Value { get; set; }
+    }
+
+    /// <summary>
+    /// The SimConnectRecvGetInputEvent structure is used to return the value of a specific input event.
+    /// </summary>
+    public struct SimConnectRecvGetInputEventHeader
+    {
+        /// <summary>
         /// Gets or sets the total size of the returned structure in bytes.
         /// </summary>
         public uint Size { get; set; }
@@ -33,10 +54,5 @@ namespace SimConnect.NET
         /// Gets or sets the type of the input event. This is used to cast the Value to the correct type.
         /// </summary>
         public SimConnectInputEventType Type { get; set; }
-
-        /// <summary>
-        /// Gets or sets the value of the requested input event, which should be cast to the correct format (float / string).
-        /// </summary>
-        public IntPtr Value { get; set; }
     }
 }
